276d5aea255f1603b754cd91fe7d962b9958c7b1
Ryan Schachte <rschacht@asu.edu>
2018-05-06 12:57:04 -0700

    Adding library handler and updating the index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const Handler = require('./lib/handler');
+
 /**
-  Handle Pull Request Specific Events
+  Handle Pull Request Specific Events To PR Handler
 */
 module.exports = (robot) => {
   robot.on(
@@ -12,24 +14,16 @@ module.exports = (robot) => {
       'pull_request.unlabeled',
       'pull_request.synchronize'
     ]
-  ), (context) => { }
+  ), (context) => { Handler.handlePullRequestEvent(context) }
 
   /**
     Handle Issue Specific Events
   */
   robot.on([
-    'issues.milestoned',
-    'issues.demilestoned',
-    'issues.renamed',
-    'issues.opened',
-  ])
-}
-
-  // robot.on('issues.opened', async context => {
-  //   // // `context` extracts information from the event, which can be passed to
-  //   // // GitHub API calls. This will return:
-  //   // //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-  //   // const params = context.issue({body: 'Hello World!'})
-  //   //
-  //   // // Post a comment on the issue
-  //   // return context.github.issues.createComment(params)
+      'issues.milestoned',
+      'issues.demilestoned',
+      'issues.renamed',
+      'issues.opened',
+    ]
+  ), (context) => { Handler.handleIssueEvent(context) }
+};
