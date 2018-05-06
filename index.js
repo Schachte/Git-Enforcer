d10067c846d42d975043597b058a1e14ae111609
Ryan Schachte <rschacht@asu.edu>
2018-05-06 12:07:56 -0700

    modifying the index fil;e

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
+module.exports = robot => {
+  robot.on('issues.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
 }
