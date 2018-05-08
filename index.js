b72b46f9b9b95d9dd4ae6202eee91ba022ee634d
Ryan Schachte <rschacht@asu.edu>
2018-05-07 19:31:42 -0700

    Refactoring, adding labels

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const Handler = require('./lib/handler');
+const EventRegistrar = require('lib/event_registrar');
 
-/**
-  Handle Pull Request Specific Events To PR Handler
-*/
+
+let prEvents = [
+  'pull_request.opened',
+  'pull_request.edited',
+  'pull_request_review.submitted',
+  'pull_request_review.edited',
+  'pull_request_review.dismissed',
+  'pull_request.labeled',
+  'pull_request.unlabeled',
+  'pull_request.synchronize'
+]
+
+let issueEvents = [
+  'issues.milestoned',
+  'issues.demilestoned',
+  'issues.renamed',
+  'issues.opened'
+]
+
 module.exports = (robot) => {
+
+/** Pull Requests */
   robot.on(
-    [ 'pull_request.opened',
-      'pull_request.edited',
-      'pull_request_review.submitted',
-      'pull_request_review.edited',
-      'pull_request_review.dismissed',
-      'pull_request.labeled',
-      'pull_request.unlabeled',
-      'pull_request.synchronize'
-    ],
+    prEvents,
     (context) => { Handler.handlePullRequestEvent(context) }
   )
 
-  /**
-    Handle Issue Specific Events
-  */
-  robot.on([
-      'issues.milestoned',
-      'issues.demilestoned',
-      'issues.renamed',
-      'issues.opened',
-    ],
+/** Issues */
+  robot.on(
+    issueEvents,
     (context) => { Handler.handleIssueEvent(context) }
   )
 };
